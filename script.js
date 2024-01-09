062440b423f1071165faff57839e24e791c944ac
Al-Aziz <79278707+Alekosh@users.noreply.github.com>
2024-01-10 01:19:40 +0500

    added visual background and upgraded logic

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
 const min = document.querySelector('#min');
 const hour = document.querySelector('#hour');
+const test = document.querySelector('.test');
+const span = document.querySelector('span')
 
 btn.addEventListener('click', () => {
   let total;
-
   if (min.checked == true) {
-    result.innerHTML = Math.floor(num.value - (num.value / speed.value)) + ' ' + 'сэкономил минут';
+    result.innerHTML = Math.floor(num.value - (num.value / speed.value)) + ' ' + `минут сэкономлено из ${num.value} минут`;
   } else {
     total = num.value * 60;
-    result.innerHTML = Math.floor(total - (total / speed.value)) + ' ' + 'сэкономил минут';
+    result.innerHTML = Math.floor(total - (total / speed.value)) + ' ' + `минут сэкономлено из ${num.value * 60} минут`;
   }
 });
+
+// num.addEventListener('blur', () => {
+//   test.innerHTML = `${num.value} минут делим на  х${speed.value}`;
+// })
+
+hour.addEventListener('click', () => {
+  span.innerHTML = 'часы';
+  num.placeholder = ' к примеру 1 час'
+})
+min.addEventListener('click', () => {
+  span.innerHTML = 'минуты';
+  num.placeholder = ' к пример 20 мин'
+})
 
