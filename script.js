7007630791ae5a854b2b0f70eda11f9f0518bc0d
Al-Aziz <79278707+Alekosh@users.noreply.github.com>
2024-01-09 06:17:33 +0500

    fixed math logic

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const num = document.querySelector('.num');
 const speed = document.querySelector('.speed');
 const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
+const min = document.querySelector('#min');
+const hour = document.querySelector('#hour');
 
 btn.addEventListener('click', () => {
-  result.innerHTML = num.value * 60 / speed.value + ' ' + "сэкономил минут";
+  let total;
+
+  if (min.checked == true) {
+    result.innerHTML = Math.floor(num.value - (num.value / speed.value)) + ' ' + 'сэкономил минут';
+  } else {
+    total = num.value * 60;
+    result.innerHTML = Math.floor(total - (total / speed.value)) + ' ' + 'сэкономил минут';
+  }
 });
 
